1d39a4b0f4a39aa967e798e2e35c08b5a137326c
nhsltn <nhsltndev@gmail.com>
2023-12-10 21:08:54 +0700

    Update Responsive Wishlist

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -8,13 +8,31 @@ const Wishlist = (Props) => {
       <Navbar />
       <div className="mt-10 max-w-7xl py-3 rounded-md border border-slate-300 max-h-full mx-auto">
         <h1 className="mx-4 text-4xl">Wishlist</h1>
-        <div className="my-5 flex justify-between mx-12 flex-wrap">
+        <div className="my-5 flex justify-between mx-12 flex-wrap md:flex-row sm:flex-col">
           <CardWishlist />
           <CardWishlist />
           <CardWishlist />
           <CardWishlist />
           <CardWishlist />
           <CardWishlist />
+        </div>
+        <div className="mx-14">
+          <button
+            type="button"
+            className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+          >
+            <span>
+              <i class="fa-solid fa-share-nodes"></i> Share Wishlist Ini
+            </span>
+          </button>
+          <button
+            type="button"
+            className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+          >
+            <span>
+              <i class="fa-solid fa-cart-plus"></i> Tambahkan Semua Item ke Cart
+            </span>
+          </button>
         </div>
       </div>
     </div>
