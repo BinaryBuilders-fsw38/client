97fa06f1d3c70fb276dbc3de01520160c0bdc544
nhsltn <nhsltndev@gmail.com>
2023-12-10 22:52:29 +0700

    Create Card Cart Component

---
+++ src/components/CardCart.jsx
@@ -0,0 +1,45 @@
+import "../css/index.css";
+
+const CardCart = (params) => {
+  return (
+    <div className="my-4 px-4">
+      <div className=" relative float-left w-20">
+        <a href="/">
+          <img
+            src="https://thumbor.sirclocdn.com/unsafe/75x92/filters:format(webp)/magento.bodypack.co.id/media/catalog/product/cache/9923e576b779a380a568c8c3f82e7a7d/b/o/bodypack_paris_2.0_high_laptop_backpack_blk-1_1024.jpg"
+            alt=""
+          />
+        </a>
+      </div>
+      <a href="/" className=" font-extrabold">
+        Bodypack Paris 2.0 High Laptop Backpack - Black
+      </a>
+
+      <div className="flex justify-between">
+        <div className="flex">
+          <p>QTY</p>
+          <button>
+            <p>
+              <i class="fa-solid fa-less-than mx-2"></i>
+            </p>
+          </button>
+          <p> 1 </p>
+          <button>
+            <p>
+              <i class="fa-solid fa-greater-than ml-2"></i>
+            </p>
+          </button>
+        </div>
+        <div>
+          <p className="font-extrabold">IDR 149,000.00</p>
+        </div>
+      </div>
+      <button className="float-right">
+        <i class="fa-solid fa-x"></i>
+      </button>
+      <hr className="flex w-11/12 mx-auto mt-3" />
+    </div>
+  );
+};
+
+export default CardCart;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import "../css/index.css";
-import Logo from '../images/everglo-logo.png'
+import Logo from "../images/everglo-logo.png";
 
 const Navbar = () => {
   return (
@@ -107,6 +107,14 @@ const Navbar = () => {
                   d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
                 />
               </svg>
+            </button>
+            <button
+              type="button"
+              className="mx-2 w-8 h-8 bg-gray-800 rounded-full focus:ring-offset-2 focus:ring-offset-gray-800"
+            >
+              <span>
+                <i class="fa-solid fa-cart-shopping text-gray-400 hover:text-white"></i>
+              </span>
             </button>
             <div className="relative ml-3">
               <div>
