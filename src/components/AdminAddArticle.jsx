97bef07e3a1f1389ae1b90fc17417c02d9ca3617
Oliver <oliversebastian321@gmail.com>
2023-12-13 00:14:41 +0700

    add articlePage admin (kurang middleware)

--- src/components/AdminAddArticle.jsx
+++ src/components/AdminAddArticle.jsx
@@ -20,11 +20,10 @@ const AddArticle = () => {
                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                   <input
                     type="text"
-                    name="username"
-                    id="username"
-                    autoComplete="username"
+                    name="tittle"
+                    id="tittle"
+                    autoComplete="tittle"
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                    placeholder="janesmith"
                   />
                 </div>
               </div>
@@ -36,8 +35,8 @@ const AddArticle = () => {
               </label>
               <div className="mt-2">
                 <textarea
-                  id="about"
-                  name="about"
+                  id="content"
+                  name="content"
                   rows={3}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   defaultValue={''}
