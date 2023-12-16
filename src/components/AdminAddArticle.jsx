d45fd249004a0d56b4bd765e6a6dd504e0ff6fa7
Oliver <oliversebastian321@gmail.com>
2023-12-16 23:38:00 +0700

    toolbar add article by admin & ganti nama MiniNavbar

--- src/components/AdminAddArticle.jsx
+++ src/components/AdminAddArticle.jsx
@@ -1,72 +1,54 @@
 import "../css/index.css";
-//import { PhotoIcon } from '@heroicons/react/24/solid'
+import React, { useState } from 'react';
+import { Editor } from "primereact/editor";
 
 const AddArticle = () => {
+  const [text, setText] = useState('');
+  
   return (
     <form>
       <div className="space-y-12 mx-auto w-full max-w-7xl sm:px-6 lg:px-8 md:px-10 xl:px-12">
         <div className="border-b border-gray-900/10 pb-12 mt-20">
-          <h1 className="text-3xl font-semibold leading-7 text-gray-900">Add Article</h1>
-          <p className="mt-1 text-sm leading-6 text-gray-600">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-          </p>
+          <h2 className="text-base font-semibold leading-7 text-gray-900">Add Article</h2>
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div className="sm:col-span-4">
-              <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-                Tittle
-              </label>
-              <div className="mt-2">
-                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <input
-                    type="text"
-                    name="tittle"
-                    id="tittle"
-                    autoComplete="tittle"
-                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-            </div>
-
-            <div className="col-span-full">
-              <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
-                Content
-              </label>
-              <div className="mt-2">
-                <textarea
-                  id="content"
-                  name="content"
-                  rows={3}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  defaultValue={''}
-                />
-              </div>
-              <p className="mt-3 text-sm leading-6 text-gray-600">Tuliskan articlenya</p>
-            </div>
-
-            {/* <div className="col-span-full">
-              <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
-                Cover photo
-              </label>
-              <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-                <div className="text-center">
-                  <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
-                  <div className="mt-4 flex text-sm leading-6 text-gray-600">
-                    <label
-                      htmlFor="file-upload"
-                      className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
-                    >
-                      <span>Upload a file</span>
-                      <input id="file-upload" name="file-upload" type="file" className="sr-only" />
-                    </label>
-                    <p className="pl-1">or drag and drop</p>
+              <div className="sm:col-span-4">
+                <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                  Tittle
+                </label>
+                <div className="mt-2">
+                  <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                    <input
+                      type="text"
+                      name="tittle"
+                      id="tittle"
+                      autoComplete="tittle"
+                      className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    />
                   </div>
-                  <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
                 </div>
               </div>
-            </div> */}
-          </div>
+
+              <div className="col-span-full">
+                <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
+                  Content
+                </label>
+                <div className="mt-2">
+                  <div className="card">
+                    <Editor value={text} onTextChange={(e) => setText(e.htmlValue)} style={{ height: '320px' }} />
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+
+        <div className="mt-6 flex items-center justify-end gap-x-6">
+            <button
+              type="submit"
+              className="rounded-md bg-gray-900 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600  mb-10"
+            >
+              Save
+            </button>
         </div>
       </div>
     </form>
--- src/components/miniNavbar.jsx
+++ src/components/miniNavbar.jsx
@@ -24,3 +24,5 @@ const MiniNavbar = () => {
 };
 
 export default MiniNavbar;
+
+//MiniNavbar
