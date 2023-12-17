ff1aa9ab9a5d4da0f088b8e1f6b2b7cdf46ff9d5
Oliver <oliversebastian321@gmail.com>
2023-12-17 21:36:05 +0700

    upate

--- src/pages/AddArticlePage.jsx
+++ src/pages/AddArticlePage.jsx
@@ -1,16 +1,11 @@
-import Footer from "../components/Footer";
-import Navbar from "../components/Nav";
 import AdminAddArtcile from "../components/AdminAddArticle";
 
 const AddArticle = () => {
   return (
     <>
-      <Navbar />
-
       <div className="LoginPage">
         <AdminAddArtcile />
       </div>
-      <Footer />
     </>
   );
 };
--- src/pages/AddProductPage.jsx
+++ src/pages/AddProductPage.jsx
@@ -1,16 +1,12 @@
 import "../css/index.css";
-import Footer from "../components/Footer";
-import Navbar from "../components/Nav";
 import FormInputProduct from "../components/FormInputProduct";
 
 const AddProduct = () => {
   return (
     <>
-      <Navbar />
       <div className="RegisterPage">
         <FormInputProduct />
       </div>
-      <Footer />
     </>
   );
 };
