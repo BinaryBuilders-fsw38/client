bd59adcb149e63f1c7f07024438fc6089a3a4edf
miasilvia <135966912+miasilvia@users.noreply.github.com>
2023-12-30 13:56:55 +0700

    menampilkan data produk dari database, dan responsive halaman produk

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -1,28 +1,43 @@
 import "../css/index.css";
-
-const CardProduct = () => {
+import React from "react";
+const CardProduct = ({ products, visible }) => {
   return (
     <>
-      <div className="max-w-xs rounded overflow-hidden px-2 py-2 mb-4 mx-2">
-        <a href="#" className="hover:opacity-75 transition duration-300">
-          <img
-            className="w-full"
-            src="https://thumbor.sirclocdn.com/unsafe/500x500/filters:format(webp)/magento.bodypack.co.id/media/catalog/product/cache/9923e576b779a380a568c8c3f82e7a7d/b/o/bodypack_credo_cross_body_cre-2_1024.jpg"
-            alt="Product"
-          />
-        </a>
-        <div>
-          <a href="#" className="MuiTypography-root" id="plugin-productTitle-typography">
-            Bodypack Credo Pouch - Cream
-          </a>
-          <div className="mt-7">
-            <p className="MuiTypography-root jss935 jss943 jss949 jss954 jss929 jss945 jss1267 price_text MuiTypography-caption MuiTypography-alignLeft">
-              IDR <b> 349,000.00 </b>
-            </p>
-          </div>
-        </div>
-      </div>
-
+      {products?.slice(0, visible).map((el, id) => {
+        return (
+          <React.Fragment key={id}>
+            <div className="max-w-xs md:w-1/5 sm:w-1/5 lg:w-1/5 xl:w-1/4 xl:w-1/5 rounded  px-2 py-2 mb-4 mx-2 h-46 lg:h-78 xl:h-90">
+              <a
+                href="#"
+                className=" hover:opacity-75 transition duration-300 w-32 md:w-25  lg:w-15 xl:w-20 h-32 lg:h-25 xl:h-48"
+              >
+                <img
+                  className=" w-20 md:w-24 lg:w-36 xl:w-48 h-20 lg:h-36 xl:h-48"
+                  src={el.product_file}
+                  alt="Product"
+                />
+              </a>
+              <div className="w-32 md:w-25  lg:w-13 xl:w-48 h-40">
+                <a
+                  href="#"
+                  className="MuiTypography-root text-xs sm:text-sm md:text-base lg:text-lg md:text-md w-13 lg:w-15 xl:w-48 h-32 lg:h-25 xl:h-48"
+                  id="plugin-productTitle-typography"
+                >
+                  <div className="xl:h-20 lg:h-28 md:h-20 sm:h-12 mt-5">
+                    {el.product_name}
+                  </div>
+                </a>
+                <div className="mt-7">
+                  <p className="MuiTypography-root MuiTypography-alignLeft text-xs sm:text-sm md:text-base lg:text-lg w-22 md:w-30  lg:w-15 xl:w-48 h-32 lg:h-25 xl:h-48">
+                    {" "}
+                    IDR <b> {el.price.toLocaleString("id-ID")} </b>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </React.Fragment>
+        );
+      })}
     </>
   );
 };
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,36 +3,83 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Nav";
 import MiniNavbar from "../components/MiniNavbar";
 import Cart from "../components/Cart";
-import { CartProvider } from '../context/CartContext';
-
+import { CartProvider } from "../context/CartContext";
+import React, { useState, useEffect, useCallback } from "react";
+import axios from "axios";
 
 const Product = () => {
+  const [dataProduct, setDataProduct] = useState([]);
+  const [brand, setBrand] = useState("");
+  const [visible, setVisible] = useState(6);
+
+  const getDataProduct = useCallback(async () => {
+    try {
+      const url = brand
+        ? `http://localhost:3000/product/get/${brand}`
+        : `http://localhost:3000/product/get`;
+      const dataProductFromServer = await axios({
+        method: "GET",
+        url: url,
+      });
+      console.log(dataProductFromServer, "ini DATA");
+      const result = dataProductFromServer.data.data;
+      setDataProduct(result);
+    } catch (error) {
+      console.log(error, "==> error");
+    }
+  }, [brand]);
+  useEffect(() => {
+    getDataProduct();
+  }, [brand, getDataProduct]);
+
+  useEffect(() => {}, [dataProduct]);
+
+  const handleBrandChange = (event) => {
+    const brand = event.target.value;
+    setBrand(brand);
+  };
+
+  const loadMore = () => {
+    // Fungsi untuk menangani klik tombol "Load More"
+    setVisible((prevValue) => prevValue + 10);
+  };
+
   return (
     <>
-    <CartProvider>
-      <Navbar />
-      <MiniNavbar />
-      <div className="absolute -mt-14">
-        <Cart />
-      </div>
+      <CartProvider>
+        <Navbar />
+        <MiniNavbar />
+        <div className="absolute">
+          <Cart />
+        </div>
       </CartProvider>
+      <div className="flex justify-end max-w-7xl mx-auto flex-wrap mb-20">
+        <select value={brand} onChange={handleBrandChange}>
+          <option value="">Semua Product</option>
+          <option value="Ustraa">Ustraa</option>
+          <option value="Elvicto">Elvicto</option>
+          <option value="HMNS">HMNS</option>
+          <option value="CAVE">CAVE</option>
+          <option value="Elvicto">Elvicto</option>
+          <option value="Kelaya">Kelaya</option>
+          <option value="Yagi">Yagi</option>
+        </select>
+      </div>
       <div className="flex justify-center max-w-7xl mx-auto flex-wrap mb-11">
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
+        <CardProduct products={dataProduct.slice(0, visible)} />
       </div>
       <div className="flex flex-col items-center mb-11">
-        <p className="mb-4">Menampilkan 10 dari 33 Produk</p>
-        <button className="border border-gray-500 py-2 px-4 w-full">
-          <b>TAMPILKAN LEBIH BANYAK 10+</b>
-        </button>
+        {}
+        
+        {visible < dataProduct.length && ( // Hanya menampilkan tombol jika masih ada produk yang belum ditampilkan
+          <button
+            onClick={loadMore}
+            className="border border-gray-500 py-2 px-4 w-full"
+          >
+            <b>TAMPILKAN SEMUA</b>
+            <p className="mb-4">Menampilkan {visible} dari {dataProduct.length} Produk</p>
+          </button>
+        )}
       </div>
       <Footer />
     </>
