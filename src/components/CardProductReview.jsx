0b058c1cfdb04d86610bf5a8ab86df534d2e45da
miasilvia <135966912+miasilvia@users.noreply.github.com>
2024-01-06 01:39:17 +0700

    membuat responsive detail produk dan menambahkan usetate untuk menu tab detail,spesifikasi,info penting

--- src/components/CardProductReview.jsx
+++ src/components/CardProductReview.jsx
@@ -1,96 +1,189 @@
-    import React from "react";
-    import { RxDotFilled } from "react-icons/rx";
-    import FotoProduct from "../images/haircare.jpg";
-    import ProductRate from "./ProductReviewRate";
-    import CardUserReview from "./UserReviewCard";
-    import { FaStar } from "react-icons/fa6";
-    const CardProduct = function () {
-    const rating = 4.5;
-    const totalReviews = 200;
-    const percentage = [10, 100, 45, 10, 25];
-    return (
-        <>
-        <div className="mt-16 ">
-            <h1 className="pt-10 pl-20 text-4xl">PRODUCT REVIEW</h1>
-            <div className="flex gap-5 mt-20 mx-20">
-            <div className="w-90">
-                <img
-                className="mx-2 my-2 rounded-md"
-                src={FotoProduct}
-                alt="product"
-                />
+import { RxDotFilled } from "react-icons/rx";
+import FotoProduct from "../images/haircare.jpg";
+import ProductRate from "./ProductReviewRate";
+import CardUserReview from "./UserReviewCard";
+import { FaStar } from "react-icons/fa6";
+import React, { useState } from "react";
+const CardProduct = function () {
+  const rating = 4.5;
+  const totalReviews = 200;
+  const percentage = [10, 100, 45, 10, 25];
+
+  const [activeTab, setActiveTab] = useState("detail");
+
+  const openTab = (tabId) => {
+    setActiveTab(tabId);
+  };
+  return (
+    <>
+      <div className="mt-16 ">
+        <h1 className="pt-10 pl-20 text-xl sm:text-1xl md:text-2xl lg:text-2xl xl:text-3xl font-bold mb-2">
+          PRODUCT REVIEW
+        </h1>
+
+        <div className="flex flex-col lg:flex-row gap-5 mt-20 mx-20">
+          <div className="lg:w-90 w-full">
+            <img
+              className="mx-2 my-2 rounded-sm md:w-2/3 sm:w-1/2 md:mx-auto sm:mx-auto w-1/2 mx-auto"
+              src={FotoProduct}
+              alt="product"
+            />
+          </div>
+          <div className="w-full ml-5">
+            <h1 className="text-xl sm:text-1xl md:text-1xl lg:text-2xl xl:text-3xl font-bold mb-2">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Voluptate,
+            </h1>
+            <div className="flex  gap-3 items-center">
+              <h4 className="font-bold">
+                Terjual <span className="text-gray-600">100+</span>
+              </h4>
+              <RxDotFilled className="mt-2" />
+              <FaStar className="text-yellow-400" />
+              <h2 className="font-bold">
+                4.7 <span className="text-gray-600">(91 rating)</span>
+              </h2>
             </div>
-            <div className="w-full ml-16">
-                <h1 className="font-bold text-2xl">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Voluptate,
-                </h1>
-                <div className="flex  gap-3 items-center">
-                <h4 className="font-bold">
-                    Terjual <span className="text-gray-600">100+</span>
-                </h4>
-                <RxDotFilled className="mt-2" />
-                <FaStar className="text-yellow-400" />
-                <h2 className="font-bold">
-                    4.7 <span className="text-gray-600">(91 rating)</span>
-                </h2>
-                </div>
-                <div className="mt-4">
-                <h1 className="text-3xl">280.000</h1>
-                <h2 className="bg-red-200 w-8 text-sm font-black text-red-600 rounded-sm px-1 flex gap-2">
-                    20% <span className="text-gray-600 line-through">Rp35.000</span>
-                </h2>
-                <div className="flex gap-16 mt-10  border-y border-gray-300">
-                    <a href="/">
-                    <h1 className="text-xl font-semibold px-2 py-2 ml-10 hover:border-b-4 hover:border-yellow-300">
-                        Detail
-                    </h1>
-                    </a>
-                    <a href="/">
-                    <h1 className="text-xl font-semibold px-2 py-2 ml-10 hover:border-b-4 hover:border-yellow-300">
-                        Spesifikasi
-                    </h1>
-                    </a>
-                    <a href="/">
-                    <h1 className="text-xl font-semibold px-2 py-2 ml-10 border-b-4 border-white hover:border-yellow-300 ">
-                        Info Penting
-                    </h1>
-                    </a>
-                </div>
-                <div className=" mb-5 mt-5 text-sm">
+            <div className="mt-4">
+              <h1 className="text-xl sm:text-1xl md:text-1xl lg:text-2xl xl:text-3xl font-bold mb-2">
+                280.000
+              </h1>
+              <h2 className="bg-red-200 w-8 text-sm font-black text-red-600 rounded-sm px-1 flex gap-2">
+                20% <span className="text-gray-600 line-through">Rp35.000</span>
+              </h2>
+              <div className="flex md:gap-2 xl:gap-20 mt-10  border-y border-gray-300">
+                <button
+                  className={`text-xl font-semibold px-2 py-2 ml-10 hover:border-b-4 hover:border-yellow-300 ${
+                    activeTab === "detail" ? "bg-black text-white" : ""
+                  }`}
+                  onClick={() => openTab("detail")}
+                >
+                  <p className="mb-4 text-sm sm:text-base md:text-lg lg:text-xl xl:text-1xl">
+                    Detail
+                  </p>
+                </button>
+                <button
+                  className={`text-xl font-semibold px-2 py-2 ml-10 hover:border-b-4 hover:border-yellow-300 ${
+                    activeTab === "spesifikasi" ? "bg-black text-white" : ""
+                  }`}
+                  onClick={() => openTab("spesifikasi")}
+                >
+                  <p className="mb-4 text-sm sm:text-base md:text-lg lg:text-xl xl:text-1xl">
+                    Spesifikasi
+                  </p>
+                </button>
+                <button
+                  className={`text-xl font-semibold px-2 py-2 ml-10 hover:border-b-4 hover:border-yellow-300 ${
+                    activeTab === "info_penting" ? "bg-black text-white" : ""
+                  }`}
+                  onClick={() => openTab("info_penting")}
+                >
+                  <p className="mb-4 text-sm sm:text-base md:text-lg lg:text-xl xl:text-1xl">
+                    Info Penting
+                  </p>
+                </button>
+              </div>
+              {activeTab === "detail" && (
+                <div id="detail" className="tab-content">
+                  <div className=" mb-5 mt-5 text-sm">
                     <h2 className="font-semibold text-gray-400">
-                    kondisi: <span className="text-black">Baru</span>
+                      kondisi: <span className="text-black">Baru</span>
                     </h2>
                     <h2 className="font-semibold text-gray-400 mt-2">
-                    Min. Pemesanan <span className="text-black"> 1 Buah</span>
+                      Min. Pemesanan <span className="text-black"> 1 Buah</span>
                     </h2>
-                    <p className="font-semibold text-sm mt-5">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Voluptas veritatis possimus, voluptatibus impedit at
-                    consequatur incidunt minus consectetur, ab autem iure error
-                    saepe natus soluta vero esse repellendus explicabo alias quis.
-                    Vel natus eos doloribus aspernatur excepturi voluptatum, ipsum
-                    neque sapiente asperiores praesentium dicta velit laboriosam
-                    esse nisi sint ea id illum quod recusandae! Aliquid quod sed
-                    excepturi ducimus adipisci. Est, deleniti totam aspernatur
-                    suscipit mollitia placeat facere magni ullam. Quod similique
-                    ut provident iusto necessitatibus veritatis nesciunt.
+                    <p
+                      className="font-semibold text-sm mt-5 mb-2 text-sm sm:text-sm md:text-md lg:text-md xl:text-lg"
+                      style={{ textAlign: "justify" }}
+                    >
+                      Ini Detail Produk Lorem ipsum dolor sit amet consectetur
+                      adipisicing elit. Voluptas veritatis possimus,
+                      voluptatibus impedit at consequatur incidunt minus
+                      consectetur, ab autem iure error saepe natus soluta vero
+                      esse repellendus explicabo alias quis. Vel natus eos
+                      doloribus aspernatur excepturi voluptatum, ipsum neque
+                      sapiente asperiores praesentium dicta velit laboriosam
+                      esse nisi sint ea id illum quod recusandae! Aliquid quod
+                      sed excepturi ducimus adipisci. Est, deleniti totam
+                      aspernatur suscipit mollitia placeat facere magni ullam.
+                      Quod similique ut provident iusto necessitatibus veritatis
+                      nesciunt.
                     </p>
+                  </div>
                 </div>
+              )}
+              {activeTab === "spesifikasi" && (
+                <div id="spesifikasi" className="tab-content">
+                  <div className=" mb-5 mt-5 text-sm">
+                    <h2 className="font-semibold text-gray-400">
+                      kondisi: <span className="text-black">Baru</span>
+                    </h2>
+                    <h2 className="font-semibold text-gray-400 mt-2">
+                      Min. Pemesanan <span className="text-black"> 1 Buah</span>
+                    </h2>
+                    <p
+                      className="font-semibold text-sm mt-5 mb-2 text-sm sm:text-sm md:text-md lg:text-md xl:text-lg"
+                      style={{ textAlign: "justify" }}
+                    >
+                      Ini spesifikasi. Lorem ipsum dolor sit amet consectetur
+                      adipisicing elit. Voluptas veritatis possimus,
+                      voluptatibus impedit at consequatur incidunt minus
+                      consectetur, ab autem iure error saepe natus soluta vero
+                      esse repellendus explicabo alias quis. Vel natus eos
+                      doloribus aspernatur excepturi voluptatum, ipsum neque
+                      sapiente asperiores praesentium dicta velit laboriosam
+                      esse nisi sint ea id illum quod recusandae! Aliquid quod
+                      sed excepturi ducimus adipisci. Est, deleniti totam
+                      aspernatur suscipit mollitia placeat facere magni ullam.
+                      Quod similique ut provident iusto necessitatibus veritatis
+                      nesciunt.
+                    </p>
+                  </div>
                 </div>
+              )}
+              {activeTab === "info_penting" && (
+                <div id="info_penting" className="tab-content">
+                  <div className=" mb-5 mt-5 text-sm">
+                    <h2 className="font-semibold text-gray-400">
+                      kondisi: <span className="text-black">Baru</span>
+                    </h2>
+                    <h2 className="font-semibold text-gray-400 mt-2">
+                      Min. Pemesanan <span className="text-black"> 1 Buah</span>
+                    </h2>
+                    <p
+                      className="font-semibold text-sm mt-5 mb-2 text-sm sm:text-sm md:text-md lg:text-md xl:text-lg"
+                      style={{ textAlign: "justify" }}
+                    >
+                      Ini info penting. Lorem ipsum dolor sit amet consectetur
+                      adipisicing elit. Voluptas veritatis possimus,
+                      voluptatibus impedit at consequatur incidunt minus
+                      consectetur, ab autem iure error saepe natus soluta vero
+                      esse repellendus explicabo alias quis. Vel natus eos
+                      doloribus aspernatur excepturi voluptatum, ipsum neque
+                      sapiente asperiores praesentium dicta velit laboriosam
+                      esse nisi sint ea id illum quod recusandae! Aliquid quod
+                      sed excepturi ducimus adipisci. Est, deleniti totam
+                      aspernatur suscipit mollitia placeat facere magni ullam.
+                      Quod similique ut provident iusto necessitatibus veritatis
+                      nesciunt.
+                    </p>
+                  </div>
+                </div>
+              )}
             </div>
-            </div>
-            <div className="flex ml-20">
-            <ProductRate
-                rating={rating}
-                percentage={percentage}
-                totalReviews={totalReviews}
-            />
-            <CardUserReview />
-            </div>
+          </div>
         </div>
-        </>
-    );
-    };
+        <div className="lg:flex ml-20">
+          <ProductRate
+            rating={rating}
+            percentage={percentage}
+            totalReviews={totalReviews}
+          />
+          <CardUserReview />
+        </div>
+      </div>
+    </>
+  );
+};
 
-    export default CardProduct;
+export default CardProduct;
--- src/components/ProductReviewRate.jsx
+++ src/components/ProductReviewRate.jsx
@@ -1,84 +1,98 @@
-    import React from 'react';
-    import { FaStar } from 'react-icons/fa6';
-    import PropTypes from 'prop-types';
+import React from "react";
+import { FaStar } from "react-icons/fa6";
+import PropTypes from "prop-types";
 
-    const Review = ({ rating, percentage, totalReviews }) => {
-    return (
-        <div className="max-w-md bg-white px-16 py-16 mt-5 ml-1 mb-5 border border-gray-300 shadow-lg rounded-lg">
-        <h1 className="text-2xl font-bold mb-4 text-center">ULASAN PEMBELI</h1>
-        <div className="flex items-center mb-4">
-            <div className="flex mr-2">
-            {[...Array(5)].map((_, index) => (
-                <span
-                key={index}
-                className={`text-2xl ${index < rating ? 'text-yellow-500' : 'text-gray-300'}`}
-                >
-                &#9733;
-                </span>
-            ))}
-            </div>
-            <span className="text-gray-600">{`${rating.toFixed(1)} (${totalReviews} reviews)`}</span>
+const Review = ({ rating, percentage, totalReviews }) => {
+  return (
+    <div className="bg-white px-12 py-16 mt-5 ml-5 mb-5 border border-gray-300 shadow-lg rounded-lg md:mr-10 mr-10 xl:mr-auto">
+      <h1 className="text-xl font-bold mb-4 text-center">ULASAN PEMBELI</h1>
+      <div className="flex items-center mb-4">
+        <div className="flex mr-2">
+          {[...Array(5)].map((_, index) => (
+            <span
+              key={index}
+              className={`text-2xl ${
+                index < rating ? "text-yellow-500" : "text-gray-300"
+              }`}
+            >
+              &#9733;
+            </span>
+          ))}
         </div>
-        <div className="mb-4">
-            <div className="flex items-center">
-            <span className="w-16 font-bold mr-2 flex gap-2 align-baseline"><FaStar className='text-yellow-400 pt-1'/> 5</span>
-            <div className="relative w-full bg-gray-200 rounded-full">
-                <div
-                className="bg-green-500 h-2 rounded-full"
-                style={{ width: `${percentage[4]}%` }}
-                ></div>
-            </div>
-            <span className="ml-2">{`${percentage[4]}%`}</span>
-            </div>
-            <div className="flex items-center">
-            <span className="w-16 font-bold mr-2 flex gap-2"><FaStar className='text-yellow-400 pt-1'/> 4</span>
-            <div className="relative w-full bg-gray-200 rounded-full">
-                <div
-                className="bg-green-500 h-2 rounded-full"
-                style={{ width: `${percentage[3]}%` }}
-                ></div>
-            </div>
-            <span className="ml-2">{`${percentage[3]}%`}</span>
-            </div>
-            <div className="flex items-center">
-            <span className="w-16 font-bold mr-2 flex gap-2"><FaStar className='text-yellow-400 pt-1'/> 3</span>
-            <div className="relative w-full bg-gray-200 rounded-full">
-                <div
-                className="bg-green-500 h-2 rounded-full"
-                style={{ width: `${percentage[2]}%` }}
-                ></div>
-            </div>
-            <span className="ml-2">{`${percentage[2]}%`}</span>
-            </div>
-            <div className="flex items-center">
-            <span className="w-16 font-bold mr-2 flex gap-2"><FaStar className='text-yellow-400 pt-1'/> 2</span>
-            <div className="relative w-full bg-gray-200 rounded-full">
-                <div
-                className="bg-green-500 h-2 rounded-full"
-                style={{ width: `${percentage[1]}%` }}
-                ></div>
-            </div>
-            <span className="ml-2">{`${percentage[1]}%`}</span>
-            </div>
-            <div className="flex items-center">
-            <span className="w-16 font-bold mr-2 flex gap-2"><FaStar className='text-yellow-400 pt-1'/> 1</span>
-            <div className="relative w-full bg-gray-200 rounded-full">
-                <div
-                className="bg-green-500 h-2 rounded-full"
-                style={{ width: `${percentage[0]}%` }}
-                ></div>
-            </div>
-            <span className="ml-2">{`${percentage[0]}%`}</span>
-            </div>
+        <span className="text-gray-600">{`${rating.toFixed(
+          1
+        )} (${totalReviews} reviews)`}</span>
+      </div>
+      <div className="mb-4">
+        <div className="flex items-center">
+          <span className="w-16 font-bold mr-2 flex gap-2 align-baseline">
+            <FaStar className="text-yellow-400 pt-1" /> 5
+          </span>
+          <div className="relative w-full bg-gray-200 rounded-full">
+            <div
+              className="bg-green-500 h-2 rounded-full"
+              style={{ width: `${percentage[4]}%` }}
+            ></div>
+          </div>
+          <span className="ml-2">{`${percentage[4]}%`}</span>
         </div>
+        <div className="flex items-center">
+          <span className="w-16 font-bold mr-2 flex gap-2">
+            <FaStar className="text-yellow-400 pt-1" /> 4
+          </span>
+          <div className="relative w-full bg-gray-200 rounded-full">
+            <div
+              className="bg-green-500 h-2 rounded-full"
+              style={{ width: `${percentage[3]}%` }}
+            ></div>
+          </div>
+          <span className="ml-2">{`${percentage[3]}%`}</span>
         </div>
-    );
-    };
+        <div className="flex items-center">
+          <span className="w-16 font-bold mr-2 flex gap-2">
+            <FaStar className="text-yellow-400 pt-1" /> 3
+          </span>
+          <div className="relative w-full bg-gray-200 rounded-full">
+            <div
+              className="bg-green-500 h-2 rounded-full"
+              style={{ width: `${percentage[2]}%` }}
+            ></div>
+          </div>
+          <span className="ml-2">{`${percentage[2]}%`}</span>
+        </div>
+        <div className="flex items-center">
+          <span className="w-16 font-bold mr-2 flex gap-2">
+            <FaStar className="text-yellow-400 pt-1" /> 2
+          </span>
+          <div className="relative w-full bg-gray-200 rounded-full">
+            <div
+              className="bg-green-500 h-2 rounded-full"
+              style={{ width: `${percentage[1]}%` }}
+            ></div>
+          </div>
+          <span className="ml-2">{`${percentage[1]}%`}</span>
+        </div>
+        <div className="flex items-center">
+          <span className="w-16 font-bold mr-2 flex gap-2">
+            <FaStar className="text-yellow-400 pt-1" /> 1
+          </span>
+          <div className="relative w-full bg-gray-200 rounded-full">
+            <div
+              className="bg-green-500 h-2 rounded-full"
+              style={{ width: `${percentage[0]}%` }}
+            ></div>
+          </div>
+          <span className="ml-2">{`${percentage[0]}%`}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-    Review.propTypes = {
-    rating: PropTypes.number.isRequired,
-    percentage: PropTypes.arrayOf(PropTypes.number).isRequired,
-    totalReviews: PropTypes.number.isRequired,
-    };
+Review.propTypes = {
+  rating: PropTypes.number.isRequired,
+  percentage: PropTypes.arrayOf(PropTypes.number).isRequired,
+  totalReviews: PropTypes.number.isRequired,
+};
 
-    export default Review;
+export default Review;
