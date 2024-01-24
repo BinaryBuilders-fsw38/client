95d02cfe19ef714d9744da9ddec3ce8094c80eab
wahyu dwi darmawan <madessu2@yahoo.co.id>
2024-01-25 02:08:33 +0700

    perbaikan wishlist

--- src/components/CardWishlist.jsx
+++ src/components/CardWishlist.jsx
@@ -3,65 +3,46 @@ import React, { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import apiUrl from "../utils/apiConfig";
 
-const CardWishlist = ({ wishlist, visible }) => {
-  const [dataProduct, setDataProduct] = useState([3]);
-  const getDataProduct = useCallback(async () => {
-    try {
-      const url = dataProduct
-        ? `${apiUrl}/product/get-product/${dataProduct}`
-        : `${apiUrl}/product/get-product`;
-      const dataProductFromServer = await axios({
-        method: "GET",
-        url: url,
-      });
-      console.log(dataProductFromServer.data.data, "DATA");
-      const result = dataProductFromServer.data.data;
-      setDataProduct(result);
-    } catch (error) {
-      console.log("------------> GAGAL");
-    }
-  }, []);
-  useEffect(() => {
-    getDataProduct();
-  }, []);
-
-  useEffect(() => {}, [dataProduct]);
-
+const CardWishlist = ({ wishlist_id, product_name, price, product_file }) => {
+  const formatCurrency = (amount) => {
+    // Menggunakan fungsi Intl.NumberFormat untuk mengubah format angka ke format mata uang
+    return new Intl.NumberFormat("id-ID", {
+      style: "currency",
+      currency: "IDR",
+    }).format(amount);
+  };
   return (
     <>
-      {dataProduct.slice().map((el, id) => {
-        return (
-          <React.Fragment key={id}>
-            <div className="card-product rounded-md border border-slate-300 flex mb-3">
-              <div className="flex align-middle justify-center flex-col w-24 md:w-28">
-                <img src={el.product_file} alt="product" />
-              </div>
-              <div className="flex align-middle justify-center flex-col w-48">
-                <a className="my-2" href="#">
-                  {el.product_name}
-                </a>
-                <p className="mb-2">IDR {el.price}</p>
-                <button
-                  type="button"
-                  className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-                >
-                  <span>
-                    <i className="fa-solid fa-cart-plus"></i> Add to Cart
-                  </span>
-                </button>
-              </div>
-              <div className="flex align-middle justify-center flex-col ">
-                <button className="py-2 px-4 rounded-full transition duration-300 ease-in-out hover:bg-slate-100">
-                  <span>
-                    <i className="fa-solid fa-trash-can"></i>
-                  </span>
-                </button>
-              </div>
-            </div>
-          </React.Fragment>
-        );
-      })}
-    </>
+    <div className="card-product rounded-md border border-slate-300 flex mb-3">
+      <div className="flex align-middle justify-center flex-col w-28">
+        <img
+          src={product_file}
+          alt={product_name}
+        />
+      </div>
+      <div className="flex align-middle justify-center flex-col w-48">
+        <a className="my-2" href="/">
+          {product_name}
+        </a>
+        <p className="mb-2">{formatCurrency(price)}</p>
+        <button
+          type="button"
+          className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+        >
+          <span>
+            <i class="fa-solid fa-cart-plus"></i> Add to Cart
+          </span>
+        </button>
+      </div>
+      <div className="flex align-middle justify-center flex-col ">
+        <button className="py-2 px-4 rounded-full transition duration-300 ease-in-out hover:bg-slate-100">
+          <span>
+            <i className="fa-solid fa-trash-can"></i>
+          </span>
+        </button>
+      </div>
+    </div>
+  </>
   );
 };
 
--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,55 +1,55 @@
 import CardWishlist from "../components/CardWishlist";
-import Navbar from "../components/Nav";
-import Cart from "../components/Cart";
 import "../css/index.css";
-import { CartProvider } from "../context/CartContext";
 import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
 import apiUrl from "../utils/apiConfig";
 
-const Wishlist = (props) => {
-  const [dataWishlist, setDataWishlist] = useState([1]);
-  const [visible, setVisible] = useState(6);
+const Wishlist = () => {
+  const [dataWishlist, setDataWishlist] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [hasFetched, setHasFetched] = useState(false);
+  const userInfo = localStorage.getItem("userInfo")
+  const userInfoObject = JSON.parse(userInfo);
+  const userId = userInfoObject.user_id 
 
-  const getDataWishlist = useCallback(async () => {
-    try {
-      const url = dataWishlist
-        ? `${apiUrl}/wishlist/get/${dataWishlist}`
-        : `${apiUrl}/wishlist/get`;
-
-      const dataWishlistFromServer = await axios({
-        method: "GET",
-        url: url,
-      });
-      console.log(dataWishlistFromServer.data.data, "ini DATA");
-      const wishlist = dataWishlistFromServer.data.data;
-      setDataWishlist(wishlist);
-    } catch (error) {
-      console.log("---------------> TIDAK BISA");
-      console.log(error, "GAGAL");
-    }
-  }, []);
+  
   useEffect(() => {
-    getDataWishlist();
-  }, []);
-
-  useEffect(() => {}, [dataWishlist]);
-
-  const loadMore = () => {
-    setVisible((prevValue) => prevValue + 10);
-  };
-
+    const getWishlistData = async() => {
+      setIsFetching(true)
+      try {
+        const response = await axios.get(`${apiUrl}/wishlist/get/${userId}`)
+        if (response.data.status === "success") {
+          console.log(response.data.data);
+          setDataWishlist(response.data.data)
+          
+          setHasFetched(true)
+        }
+      } catch {
+        // console.error("error fetching wishlist data", error)
+      } finally {
+        setIsFetching(false);
+      }
+    }
+    if(!hasFetched){
+      getWishlistData()
+      console.log(dataWishlist, "====================> data wishlist");
+    } 
+  },[hasFetched])
   return (
     <>
-      <CartProvider>
-        <div className="-mt-4 absolute">
-          <Cart />
-        </div>
-      </CartProvider>
       <div className="mt-20 max-w-7xl py-3 rounded-md border border-slate-300 max-h-full mx-auto mb-5">
         <h1 className="mx-4 mt-10 text-4xl">PRODUCT REVIEW</h1>
         <div className="my-5 flex justify-between mx-12 flex-wrap md:flex-row sm:flex-col">
-          <CardWishlist wishlist={dataWishlist.slice(0, visible)} />
+        {dataWishlist && dataWishlist.length > 0 ? (
+          dataWishlist.map((item) => (
+            <CardWishlist 
+                key={item.wishlist_id}
+                productName={item.product_name}
+                productPrice={item.price}
+                productFile={item.product_file}
+              />
+          ))
+        ) : (<p>Wishlist Anda Kosong</p>)}
         </div>
         <div className="mx-14">
           <button
@@ -69,17 +69,6 @@ const Wishlist = (props) => {
               Cart
             </span>
           </button>
-          {visible < dataWishlist.length && (
-            <button
-              onClick={loadMore}
-              className="border border-gray-500 py-2 px-4 w-full"
-            >
-              <b>TAMPILKAN SEMUA</b>
-              <p className="mb-4">
-                Menampilkan {visible} dari {dataWishlist.length} Produk
-              </p>
-            </button>
-          )}
         </div>
       </div>
     </>
