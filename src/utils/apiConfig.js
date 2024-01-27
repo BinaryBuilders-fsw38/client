3bc6fad3d8515de11ab23f2713cbb4db87cd6ac2
nhsltn <nhsltndev@gmail.com>
2024-01-27 21:31:01 +0700

    back to http, because cannot hit https in aws

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl;
 
 if (process.env.NODE_ENV === "production") {
-  apiUrl = "https://3.106.253.144";
+  apiUrl = "http://3.106.253.144";
 } else {
   apiUrl = "http://localhost:3000";
 }
